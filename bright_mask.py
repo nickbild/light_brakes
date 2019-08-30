d163a890826199eab6e3805b03e961bd3d769375
nickbild <Nicholas.Bild@sprint.com>
2019-08-30 12:37:11 -0400

    Begin LCD integration.

--- bright_mask.py
+++ bright_mask.py
@@ -1,3 +1,10 @@
+import time
+import Jetson.GPIO as GPIO
+import Adafruit_Nokia_LCD as LCD
+import Adafruit_GPIO.SPI as SPI
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 from imutils import contours
 from skimage import measure
 import numpy as np
@@ -9,10 +16,37 @@ import cv2
 min_pixels_for_masking = 300        # Min. number of connected bright pixels to get a mask.
 min_bright_pixel_value = 200        # Pixel intensity > this will be considered bright.
 
+# GPIO pin numbers.
+SCLK = 4
+DIN = 17
+DC = 23
+RST = 24
+CS = 8
+
+
+def clear_lcd(disp, image):
+	draw.rectangle((0,0,LCD.LCDWIDTH,LCD.LCDHEIGHT), outline=255, fill=255)
+	disp.image(image)
+	disp.display()
+
+
+def draw_point(disp, image):
+	draw.point((10,10), fill=None)
+	draw.point((20,20), fill=None)
+	disp.image(image)
+	disp.display()
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True)
 args = vars(ap.parse_args())
+
+# LCD initialization.
+disp = LCD.PCD8544(DC, RST, SCLK, DIN, CS)
+disp.begin(contrast=60)
+disp.clear()
+disp.display()
+lcd_image = Image.new('1', (LCD.LCDWIDTH, LCD.LCDHEIGHT))
+draw = ImageDraw.Draw(lcd_image)
 
 # Convert image to grayscale and apply blur.
 image = cv2.imread(args["image"])
@@ -50,5 +84,8 @@ for label in np.unique(labels):
 #     for c in range(len(mask[r])):
 #         print(mask[r][c])
 
-cv2.imshow("Image", mask)
-cv2.waitKey(0)
+# cv2.imshow("Image", mask)
+# cv2.waitKey(0)
+
+clear_lcd(disp, lcd_image)
+draw_point(disp, lcd_image)
