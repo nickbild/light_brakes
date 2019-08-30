405bf74d6632d4a8995b0168f52185f167b998a4
nickbild <Nicholas.Bild@sprint.com>
2019-08-30 13:19:43 -0400

    Grab image from camera.

--- bright_mask.py
+++ bright_mask.py
@@ -8,10 +8,12 @@ from PIL import ImageFont
 from imutils import contours
 from skimage import measure
 import numpy as np
-import argparse
 import imutils
 import cv2
 
+
+img_width = 84
+img_height = 48
 
 min_pixels_for_masking = 300        # Min. number of connected bright pixels to get a mask.
 min_bright_pixel_value = 200        # Pixel intensity > this will be considered bright.
@@ -36,9 +38,17 @@ def draw_point(disp, image):
 	disp.image(image)
 	disp.display()
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True)
-args = vars(ap.parse_args())
+
+def gstreamer_pipeline (capture_width=1050, capture_height=600, display_width=img_width, display_height=img_height, framerate=21, flip_method=2) :
+    return ('nvarguscamerasrc ! '
+    'video/x-raw(memory:NVMM), '
+    'width=(int)%d, height=(int)%d, '
+    'format=(string)NV12, framerate=(fraction)%d/1 ! '
+    'nvvidconv flip-method=%d ! '
+    'video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! '
+    'videoconvert ! '
+    'video/x-raw, format=(string)BGR ! appsink'  % (capture_width,capture_height,framerate,flip_method,display_width,display_height))
+
 
 # LCD initialization.
 disp = LCD.PCD8544(DC, RST, SCLK, DIN, CS)
@@ -48,44 +58,51 @@ disp.display()
 lcd_image = Image.new('1', (LCD.LCDWIDTH, LCD.LCDHEIGHT))
 draw = ImageDraw.Draw(lcd_image)
 
-# Convert image to grayscale and apply blur.
-image = cv2.imread(args["image"])
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-blurred = cv2.GaussianBlur(gray, (11, 11), 0)
+cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=2), cv2.CAP_GSTREAMER)
 
-# Find bright regions with a threshold.
-thresh = cv2.threshold(blurred, min_bright_pixel_value, 255, cv2.THRESH_BINARY)[1]
+if cap.isOpened():
+	while True:
+		ret_val, img_in = cap.read()
+		cv2.imwrite("out.jpg", img_in)
+		image = cv2.imread('out.jpg')
 
-# Remove noise.
-thresh = cv2.erode(thresh, None, iterations=2)
-thresh = cv2.dilate(thresh, None, iterations=4)
+		# Convert image to grayscale and apply blur.
+		gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+		blurred = cv2.GaussianBlur(gray, (11, 11), 0)
 
-# Connected components analysis.
-labels = measure.label(thresh, neighbors=8, background=0)
+		# Find bright regions with a threshold.
+		thresh = cv2.threshold(blurred, min_bright_pixel_value, 255, cv2.THRESH_BINARY)[1]
 
-# Loop through components.
-mask = np.zeros(thresh.shape, dtype="uint8")
-for label in np.unique(labels):
-	# Ignore background.
-	if label == 0:
-		continue
+		# Remove noise.
+		thresh = cv2.erode(thresh, None, iterations=2)
+		thresh = cv2.dilate(thresh, None, iterations=4)
 
-	# Build mask for current label.
-	labelMask = np.zeros(thresh.shape, dtype="uint8")
-	labelMask[labels == label] = 255
-	numPixels = cv2.countNonZero(labelMask)
+		# Connected components analysis.
+		labels = measure.label(thresh, neighbors=8, background=0)
 
-	# If minimum size requirement met, add current label
-    # mask to global mask.
-	if numPixels > min_pixels_for_masking:
-		mask = cv2.add(mask, labelMask)
+		# Loop through components.
+		mask = np.zeros(thresh.shape, dtype="uint8")
+		for label in np.unique(labels):
+			# Ignore background.
+			if label == 0:
+				continue
 
-# for r in range(len(mask)):
-#     for c in range(len(mask[r])):
-#         print(mask[r][c])
+			# Build mask for current label.
+			labelMask = np.zeros(thresh.shape, dtype="uint8")
+			labelMask[labels == label] = 255
+			numPixels = cv2.countNonZero(labelMask)
 
-# cv2.imshow("Image", mask)
-# cv2.waitKey(0)
+			# If minimum size requirement met, add current label
+		    # mask to global mask.
+			if numPixels > min_pixels_for_masking:
+				mask = cv2.add(mask, labelMask)
 
-clear_lcd(disp, lcd_image)
-draw_point(disp, lcd_image)
+		# for r in range(len(mask)):
+		#     for c in range(len(mask[r])):
+		#         print(mask[r][c])
+
+		# cv2.imshow("Image", mask)
+		# cv2.waitKey(0)
+
+		clear_lcd(disp, lcd_image)
+		draw_point(disp, lcd_image)
